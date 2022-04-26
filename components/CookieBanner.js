8e205a8b7d0035a59bc81f16ac7335d6aeabaf65
belowsurface3000 <f.schoeneberger@gmx.net>
2022-04-26 11:26:02 +0200

    make cookie banner work

---
+++ components/CookieBanner.js
@@ -0,0 +1,45 @@
+import { useEffect } from "react";
+import { useDispatch, useSelector } from "react-redux";
+import { acceptCookies, rejectCookies } from "../store/actions/usersActions";
+
+function CookieBanner() {
+    // redux part
+    const dispatch = useDispatch();
+    // cookie banner
+    const cookiesAccepted = (useSelector((state) => state.usersReducer.cookies));
+    const confirmCookies = () => {
+        dispatch(acceptCookies());
+        localStorage.setItem("acceptedCookies", "true");
+    };
+    const declineCookies = () => {
+        dispatch(rejectCookies());
+        localStorage.setItem("acceptedCookies", "false");
+    };
+
+    useEffect(() => {
+        if (localStorage.getItem("acceptedCookies") === "true") {
+            dispatch(acceptCookies());
+        } else if (localStorage.getItem("acceptedCookies") === "false") {
+            dispatch(rejectCookies());
+        } else {
+            dispatch(revokeCookies());
+        }
+    }, []);
+
+  return (
+    <>
+        {cookiesAccepted === null &&
+            <div className="cookie-banner">
+                <p>Diese Webseite verwendet Cookies. Mit ihrer Zustimmung werden Ihre personenbezogenen Daten verarbeitet um das Nutzererlebnis zu optimieren und zu personalisieren. Dabei kommen auch Cookies von Soundcloud und Youtube zum Einsatz. Bitte lesen Sie unsere <a href="datenschutz">Datenschutzerklärung</a> für mehr Informationen.<br/>
+                This website uses cookies. With your agreement your personal data will be used to provide you an optimized and personalized user experience. Cookies of Soundcloud and Youtube are being used and will not be loaded without your agreement.</p>
+                <div className="buttons">
+                    <button className="decline-btn" onClick={declineCookies}>Ablehnen / Decline</button>
+                    <button className="accept-btn" onClick={confirmCookies}>Annehmen / Accept</button>
+                </div>
+            </div>
+        }
+    </>
+  )
+}
+
+export default CookieBanner
--- pages/_app.js
+++ pages/_app.js
@@ -7,6 +7,7 @@ import Head from "next/head"
 import Navigation from "../components/Navigation"
 import NavigationButton from "../components/NavigationButton";
 import Background from '../components/Background';
+import CookieBanner from '../components/CookieBanner';
 
 const myStore = createStore(allReducers, composeWithDevTools());
 
@@ -14,25 +15,27 @@ function App({ Component, pageProps }) {
   return (
     <Provider store={myStore}>
       <Head>
-        <meta charset="utf-8" />
+        <meta charSet="utf-8" />
         <meta name="viewport" content="width=device-width, initial-scale=1" />
         <meta charset="UTF-8" />
         <meta name="description" content="Quality Techno and Ambient from Bonn. International artists, great supporters and state of the art music." />
         <meta name="keywords" content="Techno, Ambient, Music, Bonn" />
         <meta name="author" content="Below Surface" />
-        <meta property="og:image" content="https://besurelabel.de/pics/thumb-img.png" />
+        <meta property="og:image" content="/pics/thumb-img.png" />
         <meta property="og:url" content="https://besurelabel.de" />
         <meta property="og:type" content="website" />
         <meta property="og:title" content="Be Sure - the Techno and Ambient Label from Berlin" />
         <meta property="og:description" content="Quality music from Berlin's underground. International artists, respectable supporters and state of the art music." />
         <meta name="viewport" content="width=device-width, initial-scale=1.0" />
         <meta name="referrer" content="origin" />
-        <link rel="shortcut icon" href="https://besurelabel.de/pics/favicon.ico" type="image/x-icon" />
-        <link rel="apple-touch-icon" href="https://besurelabel.de/pics/custom-icon.png" />
+        <link rel="shortcut icon" href="/favicon.ico" type="image/x-icon" />
+        <link rel="apple-touch-icon" href="/pics/custom-icon.png" />
         <title>Be Sure | Techno & Ambient Music</title>
       </Head>
 
       <Background />
+
+      <CookieBanner />
       
       <Navigation />
       <NavigationButton />
